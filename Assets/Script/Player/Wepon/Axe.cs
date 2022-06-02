fd495ee2d587962da544417ccc5d290f59367d8c
aoyama_saito <saitomarimo1229@gmail.com>
2022-06-02 18:25:36 +0900

    [update] Axeのスクリプトを編集

--- Assets/Script/Player/Wepon/Axe.cs
+++ Assets/Script/Player/Wepon/Axe.cs
@@ -6,28 +6,29 @@ public class Axe : MonoBehaviour, IObjectPool
 {
     [SerializeField] float _movePower = 30f;
     [SerializeField] Rigidbody2D _rb;
-
-    void AxeInstantiate()
-    {
-
-    }
+    [SerializeField] SpriteRenderer _image;
 
     bool _isActrive = false;
     public bool IsActive => _isActrive;
     public void DisactiveForInstantiate()
     {
-
+        _image.enabled = false;
+        _rb.simulated = false;
     }
     public void Create()
     {
+        _image.enabled = true;
+        _rb.simulated = true;
+
         transform.position = PlayerManager.Instance.Player.transform.position;
 
-        Vector3 dir = new Vector3(Random.Range(0.5f, -0.5f), Random.Range(1f, -1f), 0).normalized;
+        Vector3 dir = new Vector3(Random.Range(0.5f, -0.5f), Random.Range(0.5f, -0.5f), 0).normalized;
         _rb.AddForce(dir * _movePower, ForceMode2D.Impulse);
     }
 
     public void Destroy()
     {
-
+        _image.enabled = false;
+        _rb.simulated = false;
     }
 }
